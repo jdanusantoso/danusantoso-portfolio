aaa86f5001fb2e2be360fab02eee8cc8a796367e
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-03-20 13:14:53 -0500

    removed comments

--- Javascript/Movie App Project 2/movie2.js
+++ Javascript/Movie App Project 2/movie2.js
@@ -101,36 +101,36 @@ const tagsEl = document.getElementById('tags')
  Genre Functions
 
 **********************/
-/* Setting up the buttons to click the genre*/
 
-//House all the clicked genres
+
+
 let selectedGenre = []
 setGenre();
 
 function setGenre() {
     tagsEl.innerHTML='';
     genres.forEach(genre => {
-        //Creating the div
+        
         const t = document.createElement('div');
-        //for each, add the tag to t
+        
         t.classList.add('tag');
-        //send the id to the genre
+        
         t.id = genre.id;
-        //Set the genre
+        
         t.innerText=genre.name;
         t.addEventListener('click', () => {
-            //If empty,
+            
             if(selectedGenre.length == 0) {
-                //push it into the empty array
+                
                 selectedGenre.push(genre.id);
             }else {
-                //If id is already there,
+                
                 if(selectedGenre.includes(genre.id)) {
-                    //loop through each id and index (idx)
+                    
                     selectedGenre.forEach((id, idx)=> {
-                        //If it is already there,
+                        
                         if(id == genre.id){
-                            //remove by splicing that specific indes value
+                            
                             selectedGenre.splice(idx, 1);
                         }
                     })
@@ -140,61 +140,60 @@ function setGenre() {
                 }
             }
                 console.log(selectedGenre)
-                //Getting info from the empty array as a string and concatenating together with url
-                //EncodeURI allows fetching movies of that genre upon clicking on it
+                
                 getMovies(API_URL + '&with_genres=' + encodeURI(selectedGenre.join(',')));
                 highlightSelection();
             
         })
-        //Show on the screen
+        
         tagsEl.append(t);
     })
 }
 
-/* Setting up in order to unhighlight the genres clicked*/
+
 
 function highlightSelection(){
-    //Select all the tags
+    
     const tags =document.querySelectorAll('.tag');
     tags.forEach(tag => {
-        //remove highlight from those classList that have it
+        
         tag.classList.remove('highlight')
     })
     clearBTN()
-    //If selected genre is not 0, 
+    
     if(selectedGenre !=0) {
-        //loop through each one
+        
         selectedGenre.forEach(id => {
-            //get the id
+            
             const highlightedTag =document.getElementById(id);
-            //add the highlighted element to that classlist
+            
             highlightedTag.classList.add('highlight');
         })
     }
 }
 
 function clearBTN(){
-    //Create clearBTN to avoid creating multiple clear btns from each click on genre
+    
     let clearBTN =document.getElementById('clear');
-    //If it exists, add highlight to its classlist
+    
     if(clearBTN){
         clearBTN.classList.add('highlight')
     } else{
-    //Create another div
+    
     let clear = document.createElement('div');
-    //Create a clear classList with tag and highlight elements added
+    
     clear.classList.add('tag', 'highlight');
-    //Create an id
+    
     clear.id = 'clear';
-    //Set the inner text to say clear
+    
     clear.innerText = 'Clear x';
     clear.addEventListener('click', () => {
-        //Reseting everything to the default dom
+        
         selectedGenre = [];
         setGenre();
         getMovies(API_URL);
     })
-    //Append to tags element
+    
     tagsEl.append(clear);
     }
     
@@ -205,13 +204,13 @@ function clearBTN(){
 getMovies(API_URL);
 
 function getMovies(url) {
-    //Fetch API: Pass in the URL through JSON, Getting the data, 
+     
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results)
-        //If the data results are more than 1, show movies
+        
         if(data.results.length !==0){
             showMovies(data.results);
-        //If not, show No Results Found
+        
         }else{
             main.innerHTML=`<h1 l class="no-results">No Results Found<\h1>`
         }
@@ -222,7 +221,7 @@ function getMovies(url) {
 
 
 function showMovies(data) {
-    //incorporating a blank slate
+    
     main.innerHTML = '';
 
     data.forEach(movie => {
@@ -230,7 +229,7 @@ function showMovies(data) {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = 
-            /*if the there is a poster, add the poster path; if not put a placeholder there*/
+            
              `<img src="${poster_path? IMG_URL + poster_path: "https://roadmap-tech.com/wp-content/uploads/2019/04/placeholder-image.jpg" }" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -242,7 +241,7 @@ function showMovies(data) {
             </div>
         
         `
-        //Appending elements to the main tag
+        
         main.appendChild(movieEl);
 
     })
@@ -265,14 +264,14 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const searchTerm = search.value;
-    //Search movies within a specified genre
+    
     selectedGenre = [];
     setGenre();
 
     if(searchTerm ) {
         getMovies(searchURL + '&query=' + searchTerm);
     }
-    //Shows the default popular movies
+    
     else{
         getMovies(API_URL);
     }
