4c50268ea78b877c3bce2c923f2f4c3135a74037
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-03-21 01:05:27 -0500

    still setting up

---
+++ React/airbnb-clone-1/airbnb-clone/src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+       <p>© 2023 Airbnb clone! No rights reserved - this is a demo!</p>
+       <p>Privacy · Terms · Sitemap · Company Details</p> 
+    </div>
+  )
+}
+
+export default Footer
